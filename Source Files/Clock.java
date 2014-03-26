372b17096cbd1a90465217cbc0bb58cecbb50dfa
Callumhazelton <callumhazelton@gmail.com>
2014-03-26 15:27:45 +0000

    Clock Class
    Added coding conventions

--- Source Files/Clock.java
+++ Source Files/Clock.java
@@ -5,50 +5,56 @@ import java.util.TimerTask;
 public class Clock {
 
     private Timer timer = new Timer();
+    private String m_TimeString;
+    int msInSec = 1000;
+    
+    public String getTimeString() {
+    	return m_TimeString;
+    }
     
     public Clock() {
-        timer.schedule(new UpdateUITask(), 0, 1000);
+        timer.schedule(new UpdateUITask(), 0, msInSec);
     }
 
     private class UpdateUITask extends TimerTask {
 
-        int nSeconds = 595;
-        String timeString;
-        int hours = 0;
-        int minutes = 0;
-        int seconds = 0;
-        
+        private int m_SecondsPassed = 00;
+        private int m_Hours = 0;
+        private int m_Minutes = 0;
+        private int m_Seconds = 0;
+        private int m_SecsInMin = 60;
+        private int m_SecsInHour = 3600;
+        private int m_doubleFigures = 10;
         
         public void run() {
             EventQueue.invokeLater(new Runnable() {
 
-                @Override
                 public void run() {
-                    nSeconds++;
-                    hours = nSeconds / 3600;
-                    minutes = (nSeconds % 3600) / 60;
-                    seconds = nSeconds % 60;
-                    String s_Seconds = Integer.toString(seconds);
-                    String s_Minutes = Integer.toString(minutes);
-                    String s_Hours = Integer.toString(hours);
-                    if (seconds<10){
-                    	s_Seconds=("0" + seconds);
+                    m_SecondsPassed++;
+                    m_Hours = m_SecondsPassed / m_SecsInHour;
+                    m_Minutes = (m_SecondsPassed % m_SecsInHour) / m_SecsInMin;
+                    m_Seconds = m_SecondsPassed % m_SecsInMin;
+                    String m_SecondsString = Integer.toString(m_Seconds);
+                    String m_MinutesString = Integer.toString(m_Minutes);
+                    String m_HoursString = Integer.toString(m_Hours);
+                    if (m_Seconds<m_doubleFigures){
+                    	m_SecondsString=("0" + m_Seconds);
                     }
-                    if (minutes<10){
-                    	s_Minutes=("0" + minutes);
+                    if (m_Minutes<m_doubleFigures){
+                    	m_MinutesString=("0" + m_Minutes);
                     }
-                    if (hours<10){
-                    	s_Hours=("0" + hours);
+                    if (m_Hours<m_doubleFigures){
+                    	m_HoursString=("0" + m_Hours);
                     }
                     
-                    timeString = s_Hours + ":" + s_Minutes + ":" + s_Seconds;
-                    System.out.println(timeString);
+                    m_TimeString = m_HoursString + ":" + 
+                    m_MinutesString + ":"  +m_SecondsString;
+                    System.out.println(m_TimeString);
                 }
             });
         }
     }
-
-    public static void main(String args[]) {
+   /* public static void main(String args[]) {
         EventQueue.invokeLater(new Runnable() {
 
             @Override
@@ -56,5 +62,5 @@ public class Clock {
                 final Clock clock = new Clock();
             }
         });
-    }
+    }*/
 }

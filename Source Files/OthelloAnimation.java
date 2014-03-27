69bb44f6d00c403357bae09758dfee4415a040ad
Ben Golightly <golightly.ben@googlemail.com>
2014-03-27 12:26:52 +0000

    Document OthelloAnimation.java

--- Source Files/OthelloAnimation.java
+++ Source Files/OthelloAnimation.java
@@ -8,16 +8,24 @@ import javax.swing.JFrame;
 import java.util.ArrayList;
 
 /**
+ * \\file -OthelloAnimation.java 
+ * \author -Ben Golightly
+ * \date -27th March 2014
  * 
- * \author Ben Golightly
- *
+ * \see Othello.java
+ * \see OthelloAnimaitonPane.java
+ * \see OthelloAnimaitonPoint.java
+ * 
+ * \brief This class manages the animation of multiple Othello pieces. 
+ * 
+ * It works by painting a "glass pane" on top of the application in order to
+ * render a list of animations onto.
  */
 public class OthelloAnimation extends Animation{
 
 	/**
-	 * Constructor method
-	 * \param frame - 
-	 * \param game - 
+	 * Constructor for an OthelloAnimation controller
+	 * \param gui a reference to the GUI, used to add the glass pane.
 	 */
 	public OthelloAnimation(GUI gui) {
 		JFrame frame = gui.GetFrame();
@@ -31,6 +39,14 @@ public class OthelloAnimation extends Animation{
 		getTimer().start();
 	}
 
+	/**
+	 * Command used to define a new point of animation to a playerColour. Used
+	 * to animate the capture of pieces by a player in Othello.
+	 * \param xcoord the X position of the piece to animate capturing
+	 * \param ycoord the Y position of the piece to animate capturing
+	 * \param playerColour the colour of the piece to animate to. Must be
+	 * 		  			   Color.black or Color.white.
+	 */
 	@Override
 	public void animate(int xcoord, int ycoord, Color playerColour) {
 		int dir;
@@ -49,10 +65,12 @@ public class OthelloAnimation extends Animation{
 	}
 
 	/**
-	 * 
+	 * called by the animation superclass at periodic intervals in order to
+	 * update the animation display.
 	 */
 	@Override
 	protected void cycle() {
+		// First remove any completed animations
 		ArrayList<OthelloAnimationPoint> toRemove =
 			new ArrayList<OthelloAnimationPoint>();
 
@@ -62,20 +80,19 @@ public class OthelloAnimation extends Animation{
 		}
 	
 		m_Animating.removeAll(toRemove);
-		//this.repaint();
+		
+		// Then set the points to animate
 		m_GlassPane.setPoints(m_Animating);
+		
+		// The draw the glass pane with these points
 		m_GlassPane.revalidate();
 		m_GlassPane.repaint();
-		// TODO m_GlassPane.setPoints(m_Animating);
 	}
-
-	public void paintComponent(Graphics g) {
-		System.out.println("OthelloAnimation::paintComponent: do");
-		// super.paintComponents(g);
-		m_GlassPane.paintComponent(g);
-	}	
 	
+	/** a reference to a glass pane to draw animations on to. */
 	private OthelloAnimationPane m_GlassPane;
+	/** currently active points to animate */
 	private ArrayList <OthelloAnimationPoint> m_Animating;
+	/** how many frames in the animation */
 	private final int FRAMES = 15; // 15 frames == 0.5 seconds
 }

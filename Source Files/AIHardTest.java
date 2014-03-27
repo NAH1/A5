ff9691b834bd3608f6eef33018b22b8861d5bffa
233max <jming.dong@gmail.com>
2014-03-27 18:26:13 +0000

    AIHardTest

---
+++ Source Files/AIHardTest.java
@@ -0,0 +1,28 @@
+package Player;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import org.junit.Test;
+
+import boardGame.BoardGame;
+import boardGame.Othello;
+
+public class AIHardTest {
+
+	@Test
+	public void test() {
+		BoardGame b = new Othello();
+		AIHard a = new AIHard("A", Color.black);
+		assertTrue(a.takeMove());
+		int x = a.getX();
+		int y =a.getY();
+		assertTrue(
+				((x == 5) && (y == 3)) ||
+				((x == 6) && (y ==4)) ||
+				((x == 3) && (y == 5)) ||
+				((x == 4) && (y ==6))
+				);
+	}
+}

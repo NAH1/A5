e48ee16ea0c8668f1365afe0521e88dc33251f20
GeraintCCFC <gerainthoward@live.co.uk>
2014-03-27 12:57:07 +0000

    AI Easy
    hope this is everything

---
+++ Source Files/Player/AIEasy.java
@@ -0,0 +1,71 @@
+package Player;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
+
+import boardGame.*;
+
+/**
+ * \\file - AIEasy.java
+ * \author Geraint Howard - 710909
+ * \date 27/03/2014
+ */
+
+public class AIEasy extends Player{
+	
+	public AIEasy(BoardGame board) {
+		m_board = board;
+	}
+	
+	private BoardGame GetBoard() {
+		return m_board;
+	}
+	
+	@Override
+	public int getX() {
+		return m_X;
+	}
+
+	@Override
+	public int getY() {
+		return m_Y;
+	}
+
+	@Override
+	public boolean isAI() {
+		return true;
+	}
+	
+	public boolean takeMove(){
+		ArrayList<int[]> moves = new ArrayList<int[]>();
+		char[][] availableMoves = ((ConnectFour) GetBoard())
+				.AvailableMove(GetPieceColour());
+		
+		for (int y = 0; y < GetBoard().GetHeight(); ++y) {
+			for (int x = 0; x < GetBoard().GetWidth(); ++x) {
+				if (availableMoves[x][y] == 'O') {
+					int score = GetBoard().MoveQuality(x,y,this);
+					int[] move = {x, y, score};
+					moves.add(move);
+				}
+			}
+		}
+		
+		
+		
+		
+		Random ran = new Random();
+		int random = ran.nextInt(moves.size());
+					
+		m_X = moves.get(random)[0];	
+		m_Y = moves.get(random)[1];
+		
+		return true;
+	}
+	
+	private int m_X;
+	private int m_Y;
+	private BoardGame m_board;
+}
+
